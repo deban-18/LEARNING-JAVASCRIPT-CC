a5aa0f8536d6cae79c0262bcdaa122753142d787
deban-18 <debanjanmandal903@gmail.com>
2025-08-16 06:31:57 +0000

    conversion of data types lec

--- basic/demo.js
+++ basic/demo.js
@@ -1,1 +1,37 @@
-console.log("Hello world")
+//console.log("Hello world")
+
+
+
+//Data Types conversion
+let name="Deb"
+let strtoNum=Number(name)
+console.log(typeof strtoNum)
+console.log(strtoNum);
+
+let roll="33"
+let strtonum1=Number(roll)
+console.log(typeof strtonum1 )
+console.log(roll);
+
+let relationship=null
+let nulltoNum=Number(relationship)
+console.log(typeof nulltoNum);
+console.log(nulltoNum);
+
+let married
+let undtoNum=Number(married)
+console.log(typeof undtoNum);
+console.log(undtoNum);
+
+let phn=8777762554
+let numtostr=String(phn)
+console.log(typeof numtostr);
+console.log(numtostr);
+
+let abv18="Arup"
+let toBool=Boolean(abv18)
+console.log(typeof toBool);
+console.log(toBool);
+
+
+
