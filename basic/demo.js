55f85c8778eb8c793c683915f20f4171df8490d4
deban-18 <debanjanmandal903@gmail.com>
2025-08-16 11:24:45 +0000

    stack heap concept

--- basic/demo.js
+++ basic/demo.js
@@ -3,35 +3,65 @@
 
 
 //Data Types conversion
-let name="Deb"
-let strtoNum=Number(name)
-console.log(typeof strtoNum)
-console.log(strtoNum);
+// let name="Deb"
+// let strtoNum=Number(name)
+// console.log(typeof strtoNum)
+// console.log(strtoNum);
 
-let roll="33"
-let strtonum1=Number(roll)
-console.log(typeof strtonum1 )
-console.log(roll);
+// let roll="33"
+// let strtonum1=Number(roll)
+// console.log(typeof strtonum1 )
+// console.log(roll);
 
-let relationship=null
-let nulltoNum=Number(relationship)
-console.log(typeof nulltoNum);
-console.log(nulltoNum);
+// let relationship=null
+// let nulltoNum=Number(relationship)
+// console.log(typeof nulltoNum);
+// console.log(nulltoNum);
 
-let married
-let undtoNum=Number(married)
-console.log(typeof undtoNum);
-console.log(undtoNum);
+// let married
+// let undtoNum=Number(married)
+// console.log(typeof undtoNum);
+// console.log(undtoNum);
 
-let phn=8777762554
-let numtostr=String(phn)
-console.log(typeof numtostr);
-console.log(numtostr);
+// let phn=8777762554
+// let numtostr=String(phn)
+// console.log(typeof numtostr);
+// console.log(numtostr);
 
-let abv18="Arup"
-let toBool=Boolean(abv18)
-console.log(typeof toBool);
-console.log(toBool);
+// let abv18="Arup"
+// let toBool=Boolean(abv18)
+// console.log(typeof toBool);
+// console.log(toBool);
+
+// let x=3
+// let y=x++  //the increment operator increments and returns the value before incrementing.
+// console.log("x: "+x  +" y: "+y);  //x: 4 y: 3
+
+// let a=3
+// let b=++a  //the increment operator increments and returns the value after incrementing.
+// console.log("a: "+a+" b: "+b);  //a: 4 b: 4
+
+
+//Stack heap concept
+let user1="Debanjan"
+let user2=user1  //in stack memeory a copy of user1 given not directly user1
+user2="rivu"    // so user 1 original value remains same 
+console.log(user1);
+console.log(user2);
+
+let user={
+    email:"debanjan229@gmail.com" ,
+    pass:1234
+}
+let anotheruser=user
+ anotheruser.email="mandaldebanjan@gmail.com"
+ console.log(user);
+ console.log(anotheruser);
+ 
+ 
+
+
+
 
 
 
