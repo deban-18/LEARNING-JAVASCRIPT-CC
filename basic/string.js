cede89bb0ab85f48038b0fd4e3d2c0dea84e308f
deban-18 <debanjanmandal903@gmail.com>
2025-08-16 12:22:57 +0000

    string lecture

---
+++ basic/string.js
@@ -0,0 +1,35 @@
+let name="Debanjan"
+let roll=133
+// console.log(`Hello my name is ${name} my roll is ${roll}`) //string interpolation
+// console.log(name[7]);
+// console.log(name.length);
+// console.log(name.toLowerCase());
+// console.log(name.charAt(3));
+// console.log(name.indexOf('j'))
+// console.log(name.substring(5)) //prints everything 5 and after 5 index means included 5
+// console.log(name.substring(0,5)) //5 excluded only upto 4 index prints
+// console.log(name.slice(0,5))
+// console.log(name.slice(-6,-5)) //If start is negative, it means length + start.If end is positive, it’s the index position (not included).end negative → treated as length + end.
+
+
+// const addrs="   98kcc mitra street   "
+// console.log(addrs);
+// console.log(addrs.trim());//remove staring ending spaces
+
+let phno="8777762554"
+// console.log(phno.replace('7','9'));
+// console.log(phno.repeat(2));
+//console.log(phno.includes("7"))
+
+let frnds="Arup Aritra Debanjan"
+console.log(frnds.split(" "));
+
+
+
+
+
+
+
+
+
+
